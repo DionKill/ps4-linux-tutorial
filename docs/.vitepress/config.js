ed3198686e581feb39c3e672debf56181e22151e
DionKill <118828188+DionKill@users.noreply.github.com>
2025-09-17 19:31:19 +0200

    Complete rewrite of the wiki
    Now the wiki is... I think hosted? Hopefully? I have no clue.
    
    Co-Authored-By: ps4boot <66526828+ps4boot@users.noreply.github.com>

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,16 @@
+export default {
+  title: "PS4 Linux Guide",
+  description: "A guide for installing Linux on any PS4",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "GitHub", link: "https://github.com/DionKill/ps4-linux-tutorial" },
+    ],
+    sidebar: [
+      { text: "Preparations", link: "/preparations" },
+      { text: "Installation", link: "/installation" },
+      { text: "Ending", link: "/ending" }
+    ]
+  }
+}
+
