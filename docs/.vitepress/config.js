fc3976cf3496f1b474d516ea1b79effb2fd296ac
DionKill <118828188+DionKill@users.noreply.github.com>
2026-01-06 14:10:01 +0100

    Rebased config.js, removed italian, sitemap fix?
    - Rebased config.js to make it more readable
    - Removed italian. I wish I had the time to maintain multiple languages, but there's no free way to maintain it, and using translator APIs just wouldn't work
    - Sitemap generation is fucked up i hope reverting all fixes it
    - Clean URLs enabled

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,4 @@
 import { defineConfig } from 'vitepress'
-import { SitemapStream } from 'sitemap'
-import { createWriteStream } from 'node:fs'
-import { resolve } from 'node:path'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -9,46 +6,11 @@ export default defineConfig({
   description: "A simple and modern guide on how to install Linux on PS4 systems.",
   base: "/ps4-linux-tutorial/",
 
-  // Metadata and favicon
-  head: [
-    // Favicon
-    ['link', { rel: 'icon', type: 'image/png', href: '/ps4-linux-tutorial/favicon.png', sizes: '32x32' }],
-    // SEO
-    ['meta', { name: 'robots', content: 'index, follow' }],
-
-    // Basic metadata
-    ['meta', { name: 'description', content: 'A step-by-step guide for running Linux on PS4' }],
-
-    // Open Graph (Discord, Facebook, etc.)
-    ['meta', { property: 'og:title', content: 'PS4 Linux Tutorial' }],
-    ['meta', { property: 'og:description', content: 'Learn how to run Linux on your PlayStation 4 with this modern and simple guide. We will go over how to setup, download kernel and distro, install, boot, customize and optimize your installation to your liking, and also fix any issues that may occur.' }],
-    ['meta', { property: 'og:type', content: 'website' }],
-    ['meta', { property: 'og:url', content: 'https://dionkill.github.io/ps4-linux-tutorial/' }],
-    ['meta', { property: 'og:image', content: 'https://dionkill.github.io/ps4-linux-tutorial/preview.png' }],
-
-    // Twitter Card
-    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
-    ['meta', { name: 'twitter:title', content: 'PS4 Linux Tutorial' }],
-    ['meta', { name: 'twitter:description', content: 'Step-by-step guide for running Linux on PS4.' }],
-    ['meta', { name: 'twitter:image', content: 'https://dionkill.github.io/ps4-linux-tutorial/preview.png' }],
-  ],
-
-  buildEnd: async ({ outDir }) => {
-    const sitemap = new SitemapStream({ hostname: 'https://dionkill.github.io/ps4-linux-tutorial/' })
-    const pages = await createContentLoader('*.md').load()
-    const writeStream = createWriteStream(resolve(outDir, 'sitemap.xml'))
-
-    sitemap.pipe(writeStream)
-    pages.forEach((page) => sitemap.write(
-      page.url
-      // If `cleanUrls is enabled`
-      .replace(/index$/g, '')
-      // Optional: if Markdown files are located in a subfolder
-      .replace(/^\/docs/, '')
-    ))
-    sitemap.end()
-
-    await new Promise((r) => writeStream.on('finish', r))
+  locales: {
+    root: {
+      label: 'English',
+      lang: 'en-US',
+    }
   },
 
   // Theme configuration
@@ -129,6 +91,11 @@ export default defineConfig({
     ],
 
     // Custom added stuff
+    siteMap: {
+      hostname: 'https://dionkill.github.io/ps4-linux-tutorial'
+    }
+
+    cleanUrls: 'true',
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/DionKill/ps4-linux-tutorial/' }
@@ -152,105 +119,27 @@ export default defineConfig({
     },
   },
 
-  locales: {
-    root: {
-      label: 'English',
-      lang: 'en-US',
-    },
-    it: {
-      label: 'Italiano',
-      lang: 'it-IT',
+  // Metadata and favicon
+  head: [
+    // Favicon
+    ['link', { rel: 'icon', type: 'image/png', href: '/ps4-linux-tutorial/favicon.png', sizes: '32x32' }],
+    // SEO
+    ['meta', { name: 'robots', content: 'index, follow' }],
 
-      // Theme configuration
-      themeConfig: {
-        // https://vitepress.dev/reference/default-theme-config
-        nav: [
-          { text: 'Home', link: '/it/' },
-          { text: 'Informazioni', link: '/it/information' },
-          {
-            text: 'Problemi',
-            items: [
-              { text: 'Problemi generali', link: '/it/issues' },
-              { text: 'Problemi per Baikal', link: '/it/baikal' },
-            ]
-          },
-          {
-            text: 'Altre cose interessanti',
-            items: [
-              { text: 'Post-installazione', link: '/it/postinstall' },
-              { text: 'Distro DIY', link: '/it/distrodiy' },
-            ]
-          },
-        ],
+    // Basic metadata
+    ['meta', { name: 'description', content: 'A step-by-step guide for running Linux on PS4' }],
 
-        sidebar: [
-          {
-            text: '📖 La Guida',
-            collapsed: false,
-            items: [
-              { text: 'Informazioni Chiave', link: '/it/information' },
-              { text: 'Revisioni e Southbridge', link: '/it/revisions' },
-              { text: 'Setup', link: '/it/setup' },
-              { text: 'File', link: '/it/files' },
-              {
-                text: '💿 Scegli il metodo di installazione',
-                collapsed: true,
-                items: [
-                  { text: 'Metodi di installazione', link: '/installation' },
-                  { text: 'Installazione interna', link: '/internal-installation' },
-                  { text: 'Installazione esterna - Script', link: '/external-installation-scripted' },
-                  { text: 'Installazione esterna - Manuale', link: '/external-installation-manual' },
-                ],
-              },
-              { text: 'Fine', link: '/ending' },
-            ]
-          },
-          {
-            text: '💜 Post Installazione',
-            collapsed: false,
-            items: [
-              { text: 'Setup post-installazione', link: '/postinstall' },
-              { text: 'Distro DIY', link: '/distrodiy' },
-              { text: 'Game testing', link: '/game-testing' },
-            ]
-          },
-          {
-            text: '💔 Problemi comuni',
-            collapsed: false,
-            items: [
-              { text: 'Problemi comuni', link: '/issues' },
-              { text: 'Problemi su Baikal', link: '/baikal' },
-            ]
-          }
-          ,
-          {
-            text: '☠️ Contenuti Legacy',
-            collapsed: true,
-            items: [
-              { text: 'Legacy e preservazione', link: '/legacy' },
-            ]
-          }
-        ],
+    // Open Graph (Discord, Facebook, etc.)
+    ['meta', { property: 'og:title', content: 'PS4 Linux Tutorial' }],
+    ['meta', { property: 'og:description', content: 'Learn how to run Linux on your PlayStation 4 with this modern and simple guide. We will go over how to setup, download kernel and distro, install, boot, customize and optimize your installation to your liking, and also fix any issues that may occur.' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:url', content: 'https://dionkill.github.io/ps4-linux-tutorial/' }],
+    ['meta', { property: 'og:image', content: 'https://dionkill.github.io/ps4-linux-tutorial/preview.png' }],
 
-        // Custom added stuff
-        cleanUrls: 'true',
-
-        socialLinks: [
-          { icon: 'github', link: 'https://github.com/DionKill/ps4-linux-tutorial/' }
-        ],
-
-        search: {
-          provider: 'local'
-        },
-
-        footer: {
-          message: 'Rilasciato sotto la <a href="https://github.com/DionKill/ps4-linux-tutorial/blob/main/LICENSE">Licenza MIT</a>.',
-        },
-
-        lastUpdated: {
-          text: 'Aggiornato il'
-        }
-      },
-    }
-  }
+    // Twitter Card
+    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { name: 'twitter:title', content: 'PS4 Linux Tutorial' }],
+    ['meta', { name: 'twitter:description', content: 'Step-by-step guide for running Linux on PS4.' }],
+    ['meta', { name: 'twitter:image', content: 'https://dionkill.github.io/ps4-linux-tutorial/preview.png' }],
+  ],
 })
--- docs/it/_includes/resume-boot-warning.md
+++
@@ -1,4 +0,0 @@
-
-> [!WARNING]
-> Don't run resume-boot more than twice, or it'll crash the system! Do `CTRL+ALT+CANC` to reboot instead!
-
